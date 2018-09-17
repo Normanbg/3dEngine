3ded1adbc5f66ff370c887e171173744aa2e5554
Normanbg <normanbg98@gmail.com>
2018-09-17 12:16:42 +0200

    Added the menu that closes the app

--- 3D_Engine/ModuleGui.cpp
+++ 3D_Engine/ModuleGui.cpp
@@ -29,32 +29,11 @@ update_status ModuleGui::PreUpdate(float dt)
 
 update_status ModuleGui::Update(float dt)
 {
-	/*ImGui::Begin("My First Tool", &my_tool_active, ImGuiWindowFlags_MenuBar);*/
-	//if (ImGui::BeginMenuBar())
-	//{
-	//	if (ImGui::BeginMenu("File"))
-	//	{
-	//		if (ImGui::MenuItem("Open..", "Ctrl+O")) { /* Do stuff */ }
-	//		if (ImGui::MenuItem("Save", "Ctrl+S")) { /* Do stuff */ }
-	//		if (ImGui::MenuItem("Close", "Ctrl+W")) { my_tool_active = false; }
-	//		ImGui::EndMenu();
-	//	}
-	//	ImGui::EndMenuBar();
-	//}
-
-	//// Edit a color (stored as ~4 floats)
-	//ImGui::ColorEdit4("Color", &my_color);
-
-	//// Plot some values
-	//const float my_values[] = { 0.2f, 0.1f, 1.0f, 0.5f, 0.9f, 2.2f };
-	//ImGui::PlotLines("Frame Times", my_values, IM_ARRAYSIZE(my_values));
-
-	//// Display contents in a scrolling region
-	//ImGui::TextColored(ImVec4(1, 1, 0, 1), "Important Stuff");
-	//ImGui::BeginChild("Scrolling");
-	//for (int n = 0; n < 50; n++)
-	//	ImGui::Text("%04d: Some text", n);
-	//ImGui::EndChild();
+	if (ImGui::BeginMenu("CLOSE"))
+	{
+		return UPDATE_STOP;
+		ImGui::EndMenu();
+	}
 	ImGui::ShowTestWindow();
 	return UPDATE_CONTINUE;
 }
