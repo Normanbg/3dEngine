fd518e303724a8d78e5f421f76a8a839ad294dea
Margeli <niniopantalones@gmail.com>
2018-09-18 16:10:06 +0200

    Added Menu Items

--- 3D_Engine/ModuleGui.cpp
+++ 3D_Engine/ModuleGui.cpp
@@ -32,10 +32,65 @@ update_status ModuleGui::PreUpdate(float dt)
 
 update_status ModuleGui::Update(float dt)
 {
-	if (ImGui::BeginMenu("CLOSE"))
+	if (ImGui::CollapsingHeader("MENU"))
 	{
-		return UPDATE_STOP;
-		ImGui::EndMenu();
+
+		if (ImGui::BeginMenu("Create")) {
+			if (ImGui::MenuItem("Sphere")) {
+				//CREATE
+				ImGui::EndMenu();
+			}
+			if (ImGui::MenuItem("Cyllinder")) {
+				//CREATE
+				ImGui::EndMenu();
+			}
+			if (ImGui::MenuItem("Capsule")) {
+				//CREATE
+				ImGui::EndMenu();
+			}
+			if (ImGui::MenuItem("AABB")) {
+				//CREATE
+				ImGui::EndMenu();
+			}
+			if (ImGui::MenuItem("OBB")) {
+				//CREATE
+				ImGui::EndMenu();
+			}
+			if (ImGui::MenuItem("Frustum")) {
+				//CREATE
+				ImGui::EndMenu();
+			}
+			if (ImGui::MenuItem("Planes")) {
+				//CREATE
+				ImGui::EndMenu();
+			}
+			if (ImGui::MenuItem("Segments")) {
+				//CREATE
+				ImGui::EndMenu();
+			}
+			if (ImGui::MenuItem("Rays")) {
+				//CREATE
+				ImGui::EndMenu();
+			}
+			if (ImGui::MenuItem("Convex Hull")) {
+				//CREATE
+				ImGui::EndMenu();
+			}
+			if (ImGui::MenuItem("Mesh")) {
+				//CREATE
+				ImGui::EndMenu();
+			}
+			if (ImGui::MenuItem("Triangles")) {
+				//CREATE
+				ImGui::EndMenu();
+			}
+			ImGui::EndMenu();
+		}		
+		if (ImGui::MenuItem("CLOSE")) {
+			return UPDATE_STOP;
+			ImGui::EndMenu();
+		}
+		//ImGui::EndMenu();
 	}
 	ImGui::ShowTestWindow();
 	return UPDATE_CONTINUE;
