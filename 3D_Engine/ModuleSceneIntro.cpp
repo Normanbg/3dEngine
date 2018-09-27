5188ae7495ad6e2c19ee4747a1467bd7e643ad2f
Margeli <niniopantalones@gmail.com>
2018-09-27 12:43:51 +0200

    test

--- 3D_Engine/ModuleSceneIntro.cpp
+++ 3D_Engine/ModuleSceneIntro.cpp
@@ -20,6 +20,11 @@ bool ModuleSceneIntro::Start()
 {
 	OWN_LOG("Loading Intro assets");
 
+	vec vertices[] = { { .0f,.0f,.0f },{ 1.0f,.0f,.0f },{ .0f,1.0f,.0f },{ 1.0f,1.0f,.0f },{ .0f,.0f,1.0f },{ 1.0f,.0f,1.0f }, { .0f,1.0f,1.0f },{ 1.0f,1.0f,1.0f } };
+	uint my_id = 0;
+	glGenBuffers(1, (GLuint*) &(my_id)); // generates 1 buffer. then it assign a GLuint to its mem adress.
+	glBindBuffer(GL_ARRAY_BUFFER, my_id); // set the type of buffer
+	glBufferData(GL_ARRAY_BUFFER, sizeof(float)*num_vertices * 3, vertices, GL_STATIC_DRAW);
 	
 	bool ret = true;
 	return ret;
@@ -36,66 +41,69 @@ bool ModuleSceneIntro::CleanUp()
 // Update
 update_status ModuleSceneIntro::Update(float dt)
 {
-	glLineWidth(10.0f);
+	/*glLineWidth(10.0f);
 	glColor4f(1.0f, 0.0f, 0.0f, 1.0f);
 	glBegin(GL_LINES);
 	glVertex3f(0.f, 0.f, 0.f);
 	glVertex3f(0.f, 20.f, 0.f);
 	glEnd();
+	glLineWidth(1.0f);*/
+	glColor4f(1.0f, 1.0f, 0.0f, 1.0f);
+	glBegin(GL_TRIANGLES);
+	glVertex3f(.0f, .0f, .0f);
+	glVertex3f(1.0f, .0f, .0f);
+	glVertex3f(.0f, 1.0f, .0f);
+	glEnd();
+
+
+
+	// --------------------------Draw Axis Grid
+	glLineWidth(2.0f);
+	glBegin(GL_LINES);
+	glColor4f(1.0f, 0.0f, 0.0f, 1.0f);
+
+	glVertex3f(0.0f, 0.0f, 0.0f); glVertex3f(1.0f, 0.0f, 0.0f);
+	glVertex3f(1.0f, 0.1f, 0.0f); glVertex3f(1.1f, -0.1f, 0.0f);
+	glVertex3f(1.1f, 0.1f, 0.0f); glVertex3f(1.0f, -0.1f, 0.0f);
+
+	glColor4f(0.0f, 1.0f, 0.0f, 1.0f);
+
+	glVertex3f(0.0f, 0.0f, 0.0f); glVertex3f(0.0f, 1.0f, 0.0f);
+	glVertex3f(-0.05f, 1.25f, 0.0f); glVertex3f(0.0f, 1.15f, 0.0f);
+	glVertex3f(0.05f, 1.25f, 0.0f); glVertex3f(0.0f, 1.15f, 0.0f);
+	glVertex3f(0.0f, 1.15f, 0.0f); glVertex3f(0.0f, 1.05f, 0.0f);
+
+	glColor4f(0.0f, 0.0f, 1.0f, 1.0f);
+
+	glVertex3f(0.0f, 0.0f, 0.0f); glVertex3f(0.0f, 0.0f, 1.0f);
+	glVertex3f(-0.05f, 0.1f, 1.05f); glVertex3f(0.05f, 0.1f, 1.05f);
+	glVertex3f(0.05f, 0.1f, 1.05f); glVertex3f(-0.05f, -0.1f, 1.05f);
+	glVertex3f(-0.05f, -0.1f, 1.05f); glVertex3f(0.05f, -0.1f, 1.05f);
+
+	glEnd();
+
+	// --------------------------Draw Plane Grid
+
 	glLineWidth(1.0f);
 
+	glBegin(GL_LINES);
+
+	glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
+	float d = 200.0f;
+
+	for (float i = -d; i <= d; i += 1.0f)
+	{
+		glVertex3f(i, 0.0f, -d);
+		glVertex3f(i, 0.0f, d);
+		glVertex3f(-d, 0.0f, i);
+		glVertex3f(d, 0.0f, i);
+	}
+
+	glEnd();
+
+	// --------------------------
 
 
-
-		// --------------------------Draw Axis Grid
-		glLineWidth(2.0f);
-
-		glBegin(GL_LINES);
-
-		glColor4f(1.0f, 0.0f, 0.0f, 1.0f);
-
-		glVertex3f(0.0f, 0.0f, 0.0f); glVertex3f(1.0f, 0.0f, 0.0f);
-		glVertex3f(1.0f, 0.1f, 0.0f); glVertex3f(1.1f, -0.1f, 0.0f);
-		glVertex3f(1.1f, 0.1f, 0.0f); glVertex3f(1.0f, -0.1f, 0.0f);
-
-		glColor4f(0.0f, 1.0f, 0.0f, 1.0f);
-
-		glVertex3f(0.0f, 0.0f, 0.0f); glVertex3f(0.0f, 1.0f, 0.0f);
-		glVertex3f(-0.05f, 1.25f, 0.0f); glVertex3f(0.0f, 1.15f, 0.0f);
-		glVertex3f(0.05f, 1.25f, 0.0f); glVertex3f(0.0f, 1.15f, 0.0f);
-		glVertex3f(0.0f, 1.15f, 0.0f); glVertex3f(0.0f, 1.05f, 0.0f);
-
-		glColor4f(0.0f, 0.0f, 1.0f, 1.0f);
-
-		glVertex3f(0.0f, 0.0f, 0.0f); glVertex3f(0.0f, 0.0f, 1.0f);
-		glVertex3f(-0.05f, 0.1f, 1.05f); glVertex3f(0.05f, 0.1f, 1.05f);
-		glVertex3f(0.05f, 0.1f, 1.05f); glVertex3f(-0.05f, -0.1f, 1.05f);
-		glVertex3f(-0.05f, -0.1f, 1.05f); glVertex3f(0.05f, -0.1f, 1.05f);
-
-		glEnd();
-
-		// --------------------------Draw Plane Grid
-
-		glLineWidth(1.0f);
-
-		glBegin(GL_LINES);
-
-		glColor4f(1.0f, 1.0f,1.0f, 1.0f);
-		float d = 200.0f;
-
-		for (float i = -d; i <= d; i += 1.0f)
-		{
-			glVertex3f(i, 0.0f, -d);
-			glVertex3f(i, 0.0f, d);
-			glVertex3f(-d, 0.0f, i);
-			glVertex3f(d, 0.0f, i);
-		}
-
-		glEnd();
-	
-		// --------------------------
-
-	
 
 
 
