9358e49997e9017838adb4bbdb204c10a78d0b7e
Normanbg <normanbg98@gmail.com>
2018-10-09 17:34:50 +0200

    3rd Parties used

--- 3D_Engine/UIPanelAbout.cpp
+++ 3D_Engine/UIPanelAbout.cpp
@@ -2,6 +2,8 @@
 #include "Application.h"
 #include "ImGui/imgui.h"
 #include "ModuleGui.h"
+#include "MathGeoLib/MathGeoLib.h"
+#include "Assimp/include/version.h"
 
 UIPanelAbout::UIPanelAbout(const char * name, float positionX, float positionY, float width, float height, bool active) : UIPanel(name,positionX,positionY, width, height, active)
 {
@@ -23,26 +25,36 @@ void UIPanelAbout::Draw(){
 	ImGui::Text("Libraries Used:");
 	SDL_version version;
 	SDL_GetVersion(&version);
-
 	char sdlVersion[30];
-	sprintf_s(sdlVersion, 30, "SDL (Version: %d.%d.%d)", version.major, version.minor, version.patch);
+	sprintf_s(sdlVersion, 30, "SDL Version: %d.%d.%d", version.major, version.minor, version.patch);
 	if (ImGui::SmallButton(sdlVersion))
 		App->RequestBrowser("https://www.libsdl.org/download-2.0.php");
-	if (ImGui::SmallButton("OpenGL"))
+	std::string glVersion = (const char*)glGetString(GL_VERSION);
+	glVersion = "OpenGL Version: " + glVersion;
+	if (ImGui::SmallButton(glVersion.c_str()))
 		App->RequestBrowser("https://www.opengl.org/");
-	if (ImGui::SmallButton("ImGui v1.65"))
+	std::string imguiVersion = ImGui::GetVersion();
+	imguiVersion = "ImGui Version: " + imguiVersion;
+	if (ImGui::SmallButton(imguiVersion.c_str()))
 		App->RequestBrowser("https://github.com/ocornut/imgui");
-	if (ImGui::SmallButton("PCG - version (C Implementation) 0.94"))
+	if (ImGui::SmallButton("PCG Version: 0.94(C Implementation)"))
 		App->RequestBrowser("http://www.pcg-random.org/");
-	if (ImGui::SmallButton("MathGeoLIB v1.5"))
+	if (ImGui::SmallButton("MathGeoLIB Version: 1.5"))
 		App->RequestBrowser("https://github.com/juj/MathGeoLib");
-	/*if (ImGui::Selectable((const char*)("Glew - version %s", glewGetString(GLEW_VERSION))))
-		App->RequestBrowser("https://github.com/nigels-com/glew");
-	*/
+	char assimpVersion[30];
+	sprintf_s(assimpVersion, 30, "Assimp Version: %d.%d.%d", aiGetVersionMajor(),aiGetVersionMinor(), aiGetVersionRevision());
+	if (ImGui::SmallButton(assimpVersion))
+		App->RequestBrowser("http://www.assimp.org/index.php/downloads");
 	char devillVersion[30];
-	sprintf_s(devillVersion, 30, "DevIL lib version %d", App->gui->ilVersion);
+	sprintf_s(devillVersion, 30, "DevIL Version %d", App->gui->ilVersion);
 	if (ImGui::SmallButton(devillVersion))
 		App->RequestBrowser("http://openil.sourceforge.net/download.php");
+	std::string glewVersion = (const char*)glewGetString(GLEW_VERSION);
+	glewVersion = "Glew Version: " + glewVersion;
+	if (ImGui::SmallButton(glewVersion.c_str()))
+		App->RequestBrowser("https://www.opengl.org/");
+	if (ImGui::SmallButton("JSON PARSON"))
+		App->RequestBrowser("http://kgabis.github.com/parson/");
 
 	ImGui::Spacing();
 	ImGui::Spacing();
