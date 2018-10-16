922566d283fb5879e1d50e1903be5bb984309bad
Margeli <niniopantalones@gmail.com>
2018-10-16 22:44:14 +0200

    filled module scene

--- 3D_Engine/ModuleScene.cpp
+++ 3D_Engine/ModuleScene.cpp
@@ -1,2 +1,26 @@
 #include "ModuleScene.h"
+#include "GameObject.h"
+
+ModuleScene::ModuleScene(bool start_enabled) : Module(start_enabled){
+
+	name = "scene";
+}
+
+bool ModuleScene::Init(JSON_Object * obj)
+{
+
+
+	return true;
+}
+
+void ModuleScene::CreateGameObject(GameObject* gameObject){
+
+	root->childrens;
+	
+}
+
+update_status ModuleScene::Update(float dt)
+{
+	return UPDATE_CONTINUE;
+}
 
--- 3D_Engine/ModuleScene.h
+++ 3D_Engine/ModuleScene.h
@@ -1,15 +1,28 @@
 #ifndef __MODULE_SCENE_H__
 #define __MODULE_SCENE_H__
 
+#include "Module.h"
 #include "Application.h"
+
 
 #include <vector>
 
 class GameObject;
 
-class ModuleScene {
+class ModuleScene : public Module
+{
 public:
-	GameObject* CreateGameObject();
+
+	ModuleScene(bool start_enabled = true);
+	~ModuleScene();
+
+	bool Init(JSON_Object* obj);
+	update_status PreUpdate(float dt);
+	update_status Update(float dt);
+	update_status PostUpdate(float dt);
+	bool CleanUp();
+
+	void CreateGameObject(GameObject* gameObject);
 
 public:
 	GameObject* root;
