859ddb60c64afedd0996ef1067cb2c687cf4ade8
Normanbg <normanbg98@gmail.com>
2018-10-23 10:38:07 +0200

    Updates on camera

--- 3D_Engine/ComponentCamera.cpp
+++ 3D_Engine/ComponentCamera.cpp
@@ -4,6 +4,14 @@
 
 ComponentCamera::ComponentCamera()
 {
+	X = vec(1.0f, 0.0f, 0.0f);
+	Y = vec(0.0f, 1.0f, 0.0f);
+	Z = vec(0.0f, 0.0f, 1.0f);
+
+	//position = transform.position
+	Reference = vec(0.0f, 0.0f, 0.0f);
+
+	LookAt(Reference);
 }
 
 
--- 3D_Engine/ComponentCamera.h
+++ 3D_Engine/ComponentCamera.h
@@ -11,14 +11,34 @@ public:
 	ComponentCamera();
 	~ComponentCamera();
 
+	bool Start();
 	bool Update() override;
 	void CleanUp()override;
 
-	void LookAt();
+	void Look(const vec &Position, const vec &Reference, bool RotateAroundReference = false);
+	void LookAt(const vec &Spot);
+	void Move(const vec &Movement);
+	void MoveTo(const vec Movement);
+
+	float* GetViewMatrix();
 
 public: 
 	Frustum camera;
+	bool freeCamera = false;
+
 	vec X, Y, Z, Position, Reference;
+	float mouseSensitivity = 0.25f;
+	float scroolWheelSensitivity = 10.0f;
+	float zoomDistance = 20.0f;
+
+private:
+	void CalculateViewMatrix();
+
+private:
+	bool debug = false;
+	vec offset_to_player;
+	float4x4 ViewMatrix, ViewMatrixInverse;
+
 };
 
 #endif // !__COMPONENTCAMERA_H__
