d72c4b77da06709b334a6af363249bde3c333798
Normanbg <normanbg98@gmail.com>
2018-10-24 16:56:28 +0200

    Commented for not crashing

--- 3D_Engine/ComponentCamera.h
+++ 3D_Engine/ComponentCamera.h
@@ -1,47 +1,47 @@
-#ifndef __COMPONENTCAMERA_H__
-#define __COMPONENTCAMERA_H__
-
-#include "Globals.h"
-#include "Application.h"
-#include "Component.h"
-#include "Math.h"
-
-class ComponentCamera : public Component
-{
-public:
-	ComponentCamera();
-	~ComponentCamera();
-
-	bool Start();
-	bool Update() override;
-	void CleanUp()override;
-
-	void Look(const float3 &Position, const float3 &Reference, bool RotateAroundReference = false);
-	void LookAt(const float3 &Spot);
-	void Move(const float3 &Movement);
-	void MoveTo(const float3 Movement);
-
-	void SetNearPlaneDistance(float nearPlaneDist);
-	void SetFarPlaneDistance(float farPlaneDist);
-
-	float GetNearPlaneDistance() const { return camFrustum.NearPlaneDistance; }
-	float GetFarPlaneDistance() const { return camFrustum.FarPlaneDistance; }
-
-public: 
-	Frustum camFrustum;
-
-	float3 X, Y, Z, Position, Reference;
-	float mouseSensitivity = 0.25f;
-	float scroolWheelSensitivity = 10.0f;
-	float zoomDistance = 20.0f;
-
-
-private:
-	void CalculateViewMatrix();
-
-private:
-	float4x4 ViewMatrix, ViewMatrixInverse;
-
-};
-
-#endif // !__COMPONENTCAMERA_H__
+//#ifndef __COMPONENTCAMERA_H__
+//#define __COMPONENTCAMERA_H__
+//
+//#include "Globals.h"
+//#include "Application.h"
+//#include "Component.h"
+//#include "Math.h"
+//
+//class ComponentCamera : public Component
+//{
+//public:
+//	ComponentCamera();
+//	~ComponentCamera();
+//
+//	bool Start();
+//	bool Update() override;
+//	void CleanUp()override;
+//
+//	void Look(const float3 &Position, const float3 &Reference, bool RotateAroundReference = false);
+//	void LookAt(const float3 &Spot);
+//	void Move(const float3 &Movement);
+//	void MoveTo(const float3 Movement);
+//
+//	void SetNearPlaneDistance(float nearPlaneDist);
+//	void SetFarPlaneDistance(float farPlaneDist);
+//
+//	float GetNearPlaneDistance() const { return camFrustum.NearPlaneDistance; }
+//	float GetFarPlaneDistance() const { return camFrustum.FarPlaneDistance; }
+//
+//public: 
+//	Frustum camFrustum;
+//
+//	float3 X, Y, Z, Position, Reference;
+//	float mouseSensitivity = 0.25f;
+//	float scroolWheelSensitivity = 10.0f;
+//	float zoomDistance = 20.0f;
+//
+//
+//private:
+//	void CalculateViewMatrix();
+//
+//private:
+//	float4x4 ViewMatrix, ViewMatrixInverse;
+//
+//};
+//
+//#endif // !__COMPONENTCAMERA_H__
