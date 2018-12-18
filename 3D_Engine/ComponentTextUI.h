ebc2429966c0d3b8e2336bc333f5495e8a43d139
Margeli <niniopantalones@gmail.com>
2018-12-18 17:51:56 +0100

    working on fonts ( now loaded but with visual errors)

--- 3D_Engine/ComponentTextUI.h
+++ 3D_Engine/ComponentTextUI.h
@@ -12,8 +12,11 @@ public:
 
 	void DrawInspector() override;
 
+	bool Start() override;
 	bool Update() override;
 	void CleanUp() override;	
+
+	inline void doStart() override { Start(); }
 	inline void doUpdate() override { Update(); }
 	inline void doCleanUp() override { CleanUp(); }
 
@@ -21,6 +24,12 @@ public:
 	void Save(Config& data) const override;
 	inline void doLoad(Config* data) override { Load(data); }
 	inline void doSave(Config& data)const  override { Save(data); }
+
+
+	// txt RESOURCE?
+	std::string fontPath = std::string(FONTS_PATH);
+	float scale = 0.0f;
+	char* text = nullptr;
 };
 
 
--- 3D_Engine/Globals.h
+++ 3D_Engine/Globals.h
@@ -73,15 +73,17 @@ enum FrustumContained {
 #define ENGINE_FX_PATH "Engine/Audio/Fx/"
 #define CAMERA_SPEED 10.0f
 
-#define NUM_PATHS 8
 #define TEXTURES_PATH "Assets/Textures/"
 #define MODELS_PATH "Assets/3DModels/"
 #define AUDIO_PATH "Assets/Audio/"
+#define FONTS_PATH "Assets/Fonts/"
 #define LIB_TEXTURES_PATH "Library/Textures/"
 #define LIB_MODELS_PATH "Library/3DModels/"
 #define LIB_AUDIO_PATH "Library/Audio/"
 #define SETTINGS_PATH "Settings/"
 #define SCENES_PATH "Scenes/"
+#define NUM_PATHS 9
+
 #define ASSETS_PATH "Assets/"
 #define OWN_FILE_FORMAT ".pei"
 #define OWN_FILE_FORMAT_CAP ".PEI"
